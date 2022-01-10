15be18159dfabb55c560459a3602b68d450ebe60
yburakgunes <yburakgunes93@gmail.com>
2022-01-10 09:52:22 +0300

    Spring Boot & Spring MVC

---
+++ src/main/java/com/burakgunes/SpringBootBilgeAdamApplication.java
@@ -0,0 +1,13 @@
+package com.burakgunes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootBilgeAdamApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootBilgeAdamApplication.class, args);
+	}
+
+}
